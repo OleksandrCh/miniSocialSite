599641590a6c2c4dac6530ce1c8a021593f57a3b
OleksandrCh <oleksandrchernii.0@gmail.com>
2020-03-19 01:57:53 +0200

    Add state.js

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ import Profile from "./component/Profile/Profile";
 import Dialogs from "./component/Dialogs/Dialogs.jsx";
 import {BrowserRouter, Route} from "react-router-dom";
 
-const App = ({postData,dialogsData,messageData}) => {
+const App = ({state:{profilePage,dialogsPage}}) => {
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
                 <Header/>
                 <Nav/>
                 <div className='app-wrapper-content'>
-                    <Route exact path='/profile' render={()=><Profile postData={postData}/>}/>
-                    <Route exact path='/dialogs' render={()=><Dialogs dialogsData={dialogsData} messageData={messageData}/>}/>
-                    <Route exact path='/news' render={()=><News/>}/>
-                    <Route exact path='/music' render={()=><Music/>}/>
-                    <Route exact path='/settings' render={()=><Settings/>}/>
+                    <Route exact path='/profile' render={()=><Profile postData={profilePage.postData}/>}/>
+                    <Route exact path='/dialogs' render={()=><Dialogs dialogsData={dialogsPage.dialogsData} messageData={dialogsPage.messageData}/>}/>
+                    {/*<Route exact path='/news' render={()=><News/>}/>*/}
+                    {/*<Route exact path='/music' render={()=><Music/>}/>*/}
+                    {/*<Route exact path='/settings' render={()=><Settings/>}/>*/}
                 </div>
             </div>
         </BrowserRouter>
---
+++ src/redux/state.js
@@ -0,0 +1,27 @@
+export let state = {
+    profilePage: {
+        postData: [
+            {post: 'Hi, how are you?'},
+            {post: 'It\'s my first post!'},
+            {post: 'Good by!'},
+        ],
+    },
+    dialogsPage: {
+        dialogsData: [
+            {id: 1, name: 'Dimuch'},
+            {id: 2, name: 'Andrei'},
+            {id: 3, name: 'Sveta'},
+            {id: 4, name: 'Sasha'},
+            {id: 5, name: 'Viktor'},
+            {id: 6, name: 'Valera'}
+        ],
+        messageData: [
+            {id: 1, message: 'Hi how are you?'},
+            {id: 2, message: 'Were are you from?'},
+            {id: 3, message: 'Hi how are you?'},
+            {id: 4, message: 'By!'},
+            {id: 5, message: 'Yo!'}
+        ],
+    }
+
+};
